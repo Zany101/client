c4d2a56598e2a57c10b4ded396c17e70868ebd0f
Danny Verhoeven <d.verhoeven95@live.nl>
2021-08-23 01:57:11 +0200

    Preparing user account pages.

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -13,8 +13,22 @@ class AccountController extends AbstractController
      */
     public function index(): Response
     {
+      $user = $this->getUser();
+
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/login-credentials", name="login-credentials")
+     */
+    public function credentials(): Response
+    {
+      $user = $this->getUser();
+
+        return $this->render('account/index.html.twig', [
+            'user' => $user,
         ]);
     }
 }
